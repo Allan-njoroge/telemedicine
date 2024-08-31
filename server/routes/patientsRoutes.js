9e0bbc22c9a9c7e288d80c16cbbc0865b587fc13
Allan-njoroge <allandavis254@gmail.com>
2024-08-31 20:47:50 +0300

    patient routes

---
+++ server/routes/patientsRoutes.js
@@ -0,0 +1,18 @@
+import express from "express"
+import { getPatients, getPatientsById, getPatientsByName } from "../controllers/patientsController.js"
+
+const router = express.Router()
+
+// get all patents
+router.get('', getPatients)
+
+//get patient by id
+router.get('/id/:id', getPatientsById)
+
+// get patient by name
+router.get('/name', getPatientsByName)
+
+// get all patients by gender
+router.get('/gender/:gender')
+
+export default router
