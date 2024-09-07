926b9e4ac7a24cc04bca875a452228d314b48078
Allan-njoroge <allandavis254@gmail.com>
2024-09-07 12:13:23 +0300

    Patiens panel frontend initialization

---
+++ patient-client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <div className="h-[100vh] w-full flex justify-center items-center">
+      <h1>Telemedicine App - Patient's Panel</h1>
+      <Button>Click Me</Button>
+    </div>
+  )
+}
+
+export default App
