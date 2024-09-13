1ce4614422b8b6c48a682b91b703e747e8a316c4
Allan-njoroge <allandavis254@gmail.com>
2024-09-13 19:06:19 +0300

    Routing configuration

--- patient-client/src/App.jsx
+++ patient-client/src/App.jsx
@@ -1,15 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
 
 function App() {
   return (
-    <div className="relative">
-      <Navbar  />
-      <Home className="mt-20" />
-    </div>
-  )
+    <>
+      <Navbar />
+      <Routes>
+        {/* Routes */}
+        <Route element={<Home className="mt-20" />} path="/" />
+        <Route element={<Doctors />} path="/doctors"/>
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
