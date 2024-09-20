472836dc64af3f8519f35c298c499aeeb8989fb2
Allan-njoroge <allandavis254@gmail.com>
2024-09-20 21:36:25 +0300

    Updated routing in the main app file

--- patient-client/src/App.jsx
+++ patient-client/src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Doctors from "./pages/Doctors";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -11,7 +13,11 @@ function App() {
       <Routes>
         {/* Routes */}
         <Route element={<Home className="mt-20" />} path="/" />
-        <Route element={<Doctors />} path="/doctors"/>
+        <Route element={<Doctors />} path="/doctors" />
+        <Route element={<About />} path="/about"/>
+        <Route element={<Contact />} path="/contact" />
+
+        <Route path="*" element={ <div><h1>this page is not found</h1></div>}/>
       </Routes>
     </>
   );
