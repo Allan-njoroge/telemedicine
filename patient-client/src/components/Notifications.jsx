1b86767f4d24c2f78fae4b7da5726b31afb88969
Allan-njoroge <allandavis254@gmail.com>
2024-10-11 21:03:44 +0300

    The notification on the side panel for the patient

---
+++ patient-client/src/components/Notifications.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notifications = () => {
+  return (
+    <div className="w-[30%] bg-primary h-[95vh] mt-[2.5vh] mr-4 rounded-md">
+        Notifications
+    </div>
+  )
+}
+
+export default Notifications
